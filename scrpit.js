000ca9705319860574a5f132fa8c4adf6be69e78
kulewers <kule.wers@gmail.com>
2022-10-26 19:29:36 +0300

    Reinvent logic, add score and winner announcement

--- scrpit.js
+++ scrpit.js
@@ -12,30 +12,36 @@ function getComputerChoice() {
 }
 
 function playRound (playerSelection, computerSelection) {
+    if (winner.textContent === 'Victory!' || winner.textContent === 'You lost!') {
+        return;
+    }
     if (playerSelection === 'rock') {
         if (computerSelection === 'rock') {
             div.textContent = 'Tie! Rock versus Rock';
         } else if (computerSelection === 'paper') {
             div.textContent = 'You Lose! Paper beats Rock';
+            addScore('loss');
         } else {
             div.textContent = 'You Win! Rock beats Scissors';
-            runningScore.textContent = parseInt(runningScore.textContent) + 1;
+            addScore('win');
         }
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
             div.textContent = 'You Win! Paper beats Rock';
-            runningScore.textContent = parseInt(runningScore.textContent) + 1;
+            addScore('win');
         } else if (computerSelection === 'paper') {
             div.textContent = 'Tie! Paper versus Paper';
         } else {
             div.textContent = 'You Lose! Scissors beats Paper';
+            addScore('loss')
         }
     } else {
         if (computerSelection === 'rock') {
             div.textContent = 'You Lose! Rock beats Scissors';
+            addScore('loss');
         } else if (computerSelection === 'paper') {
             div.textContent = 'You Win! Scissors beats paper';
-            runningScore.textContent = parseInt(runningScore.textContent) + 1;
+            addScore('win');
         } else {
             div.textContent = 'Tie! Scissors versus Scissors';
         }
@@ -43,24 +49,37 @@ function playRound (playerSelection, computerSelection) {
 }
 
 const runningScore = document.querySelector('div.runningScore');
-runningScore.textContent = 0;
+runningScore.textContent = '0 - 0';
+
+function addScore(roundResult) {
+    let newScore = runningScore.textContent.split(' - ');
+    if (roundResult === 'win') {
+        newScore[0]++;
+    } else {
+        newScore[1]++;
+    }
+    if (newScore[0] == '5') {
+        winner.textContent = 'Victory!';
+    } else if (newScore[1] == '5') {
+        winner.textContent = 'You lost!';
+    }
+    runningScore.textContent = newScore.join(' - ');
+}
 
 const button1 = document.querySelector("#rock");
 const button2 = document.querySelector("#paper");
 const button3 = document.querySelector("#scissors");
 
+const winner = document.querySelector('div.winnerAnnouncement');
 const div = document.querySelector('div.roundResult');
 
 button1.addEventListener('click', function() {
     playRound('rock', getComputerChoice());
 });
 button2.addEventListener('click', function() {
-    playRound('peper', getComputerChoice())
+    playRound('paper', getComputerChoice())
 });
 button3.addEventListener('click', function() {
     playRound('scissors', getComputerChoice());
 });
-
-
-
 
